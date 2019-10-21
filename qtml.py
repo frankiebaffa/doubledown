45418390f4a60a0fa905fad8c4011544c6138f53
frankiebaffa <frankiebaffa@gmail.com>
2019-10-21 19:37:00 -0400

    master: further parsing of .qtml files

--- qtml.py
+++ qtml.py
@@ -7,6 +7,21 @@ class QtmlElement:
     qattributes = None
     qinner      = None
 
+    def __init__(self):
+        self.qtag        = None
+        self.qid         = None
+        self.qclass      = []
+        self.qattributes = []
+        self.qinner      = []
+
+    def __repr__(self):
+        printid = None
+        if self.qid == None:
+            printid = "No ID"
+        else:
+            printid = self.qid
+        return f"<{self.qtag} | {printid} | classes:{len(self.qclass)}>"
+
 class QtmlDocument:
     qtmlelements = []
 
@@ -38,23 +53,51 @@ class QtmlParser:
                 elementend   = line.index('|')
             except:
                 print(f'Malformed QTML on line {i}')
-            elementstr  = line[elementstart:elementend]
-            qelement    = QtmlElement()
-
-            qelement.qattributes\
-           ,elementstr           = QtmlParser.checkGetAttr(elementstr)
-            qelement.qclass\
-           ,elementstr           = QtmlParser.checkGetClass(elementstr)
+            if elementstart < elementend:
+                elementstr  = line[elementstart+1:elementend]
+                qelement    = QtmlElement()
+                qelement,\
+                elementstr  = QtmlParser.checkGetName(elementstr,qelement)
+                qelement,\
+                elementstr  = QtmlParser.checkGetAttr(elementstr,qelement)
+                qelement,\
+                elementstr  = QtmlParser.checkGetClass(elementstr,qelement)
+                print(qelement)
+            else:
+                # nothing yet
+                continue
 
     @staticmethod
-    def checkGetAttr(elementstr):
-        attrs = re.findall(r"\[[a-zA-Z0-9=,]+\]",elementstr)
-        print(attrs)
-        return False,elementstr
+    def checkGetName(elementstr,qelement):
+        name = re.findall(r"^[a-zA-Z0-9]+",elementstr)
+        qelement.qtag = name[0]
+        elementstr = elementstr.replace(name[0],'',1)
+        return qelement,elementstr
 
     @staticmethod
-    def checkGetClass(elementstr):
+    def checkGetAttr(elementstr,qelement):
+        attrs = re.findall(r"(?<=\[)[a-zA-Z0-9=,\.]+(?=\])",elementstr)
+        for match in attrs:
+            elementstr = elementstr.replace(match,'',1)
+            attrsplit = match.split(',')
+            for attr in attrsplit:
+                kv = attr.split('=')
+                if len(kv) == 1:
+                    qelement.qattributes.append(kv[0])
+                elif len(kv) == 2:
+                    qelement.qattributes.append({kv[0]:kv[1]})
+        elementstr = elementstr.replace('[','')
+        elementstr = elementstr.replace(']','')
+        return qelement,elementstr
+
+    @staticmethod
+    def checkGetClass(elementstr,qelement):
         classstart = None
         classes = re.findall(r"(?<=\.)[a-zA-Z0-9]+",elementstr)
-        print(classes)
-        return False,elementstr
+        for qclass in classes:
+            elementstr = elementstr.replace(qclass,'',1)
+            qelement.qclass.append(qclass)
+        return qelement,elementstr
+
+if __name__ == '__main__':
+    QtmlParser('./test.qtml')
