074197db6816b49841a202910423cb3d6e7c058b
Frankie <frankiebaffa@gmail.com>
2019-10-23 12:52:38 -0400

    master: alter scripts

--- scripts/movevimfiles.sh
+++ scripts/movevimfiles.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo rm /usr/local/share/vim/vim81/syntax/qtml.vim
-sudo cp ./vim/qtml.vim /usr/local/share/vim/vim81/syntax/qtml.vim
+sudo rm /usr/share/vim/vim80/syntax/qtml.vim
+sudo cp ./vim/qtml.vim /usr/share/vim/vim80/syntax/qtml.vim
 
 rm ~/.vim/syntax/qtml.vim
 cp ./vim/qtml.vim ~/.vim/syntax/qtml.vim
