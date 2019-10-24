d41af42b73cbe2b2eade6f25ebb6b89cbb2164fd
Frankie <frankiebaffa@gmail.com>
2019-10-24 13:38:03 -0400

    master: add / require output argument

--- optionparser.py
+++ optionparser.py
@@ -11,13 +11,14 @@ def parseOpts(argv):
                     "layoutfile" : None,
                     "cssfile"    : None,
                     "singlefile" : None,
+                    "output"     : None,
                     "quiet"      : False,
                     "html"       : False
                   }
     try:
         opts,args = getopt.getopt(
             argv,
-            "c:l:s:i:qh",
+            "c:l:s:i:o:qh",
             [
                 "contentfile=",
                 "layoutfile=",
@@ -28,6 +29,7 @@ def parseOpts(argv):
     except getopt.GetoptError:
         print('Error getting opts')
         sys.exit(2)
+    hasOut = False
     for opt,arg in opts:
         if opt == '-c':
             options["contentfile"] = arg
@@ -41,4 +43,10 @@ def parseOpts(argv):
             options["quiet"] = True
         elif opt == '-h':
             options["html"] = True
+        elif opt == '-o':
+            options["output"] = arg
+            hasOut = True
+    if not hasOut:
+        print("Include -o switch and define output file.")
+        sys.exit(2)
     return options
