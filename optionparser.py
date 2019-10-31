a4de746b0e66f91a538415e0ff8625bff79e67ba
Frankie <frankiebaffa@gmail.com>
2019-10-31 07:11:27 -0400

    master: move error logic to doubledown.py

--- optionparser.py
+++ optionparser.py
@@ -6,44 +6,39 @@ def parseOpts(argv):
     layoutfile  = None
     cssfile     = None
     opts        = None
-    options     = {"contentfile"  : None,
+    options     = {
+                   "contentfile"  : None,
                    "layoutfile"   : None,
                    "cssfile"      : None,
                    "singlefile"   : None,
                    "output"       : None,
                    "quiet"        : False,
-                   "html"         : False}
+                   "html"         : False
+                  }
     try:
         opts,args = getopt.getopt(
             argv,
             "c:l:s:i:o:qh",
-            ["contentfile=",
+            [
+             "contentfile=",
              "layoutfile=",
              "cssfile=",
              "singlefile=",
-             "outputfile="]
+             "outputfile="
+            ]
         )
+
+        for opt,arg in opts:
+            if   opt == '-c': options["contentfile"] = arg
+            elif opt == '-l': options["layoutfile"] = arg
+            elif opt == '-s': options["cssfile"] = arg
+            elif opt == '-i': options["singlefile"] = arg
+            elif opt == '-q': options["quiet"] = True
+            elif opt == '-h': options["html"] = True
+            elif opt == '-o':
+                options["output"] = arg
+
     except getopt.GetoptError:
-        print('Error getting opts')
-        sys.exit(2)
-    hasOut = False
-    for opt,arg in opts:
-        if opt == '-c':
-            options["contentfile"] = arg
-        elif opt == '-l':
-            options["layoutfile"] = arg
-        elif opt == '-s':
-            options["cssfile"] = arg
-        elif opt == '-i':
-            options["singlefile"] = arg
-        elif opt == '-q':
-            options["quiet"] = True
-        elif opt == '-h':
-            options["html"] = True
-        elif opt == '-o':
-            options["output"] = arg
-            hasOut = True
-    if not hasOut:
-        print("Include -o switch and define output file.")
-        sys.exit(2)
+        options = None
+
     return options
