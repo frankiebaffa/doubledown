dd04b2f91285c621f0d074b1864b042cbee3a41d
Frankie <frankiebaffa@gmail.com>
2019-12-05 07:51:48 -0500

    htmlSyntax: add regexHas and regexHasOne fns

--- utils/utils.py
+++ utils/utils.py
@@ -3,3 +3,10 @@ def arrFind(p,a):
         return a.index(p)
     except:
         return -1
+
+import re
+def regexHas(p,s):
+    return len(re.findall(p,s)) > 0
+
+def regexHasOne(p,s):
+    return len(re.findall(p,s)) == 1
