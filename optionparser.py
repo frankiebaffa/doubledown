3091c94e43e08d95acb66561818d01aa30b4e149
Frankie <frankiebaffa@gmail.com>
2019-12-06 15:17:12 -0500

    htmlSyntax: reformat file

--- optionparser.py
+++ optionparser.py
@@ -2,30 +2,35 @@ import getopt
 import sys
 
 def parseOpts(argv):
-    opts        = None
-    options     = {
-                   "singlefile"   : None,
-                   "output"       : None,
-                   "quiet"        : False,
-                   "html"         : False,
-                   "test"         : False
-                  }
+    opts = None
+    options = {
+            "singlefile" : None,
+            "output" : None,
+            "quiet" : False,
+            "html" : False,
+            "test" : False
+            }
     try:
         opts,args = getopt.getopt(
-            argv,
-            "i:o:qht",
-            [
-             "singlefile=",
-             "outputfile="
-            ]
-        )
+                argv,
+                "i:o:qht",
+                [
+                    "singlefile=",
+                    "outputfile="
+                    ]
+                )
 
         for opt,arg in opts:
-            if   opt == '-i': options["singlefile"] = arg
-            elif opt == '-o': options["output"] = arg
-            elif opt == '-q': options["quiet"] = True
-            elif opt == '-h': options["html"] = True
-            elif opt == '-t': options["test"] = True
+            if opt == '-i':
+                options["singlefile"] = arg
+            elif opt == '-o':
+                options["output"] = arg
+            elif opt == '-q':
+                options["quiet"] = True
+            elif opt == '-h':
+                options["html"] = True
+            elif opt == '-t':
+                options["test"] = True
 
     except getopt.GetoptError:
         options = None
